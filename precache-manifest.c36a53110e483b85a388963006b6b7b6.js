db3558f90b9bcce8e053057ffeaf9d72cdb2a196
Roy McKenzie <roymckenzie4@gmail.com>
2022-04-25 23:27:40 -0500

    Updates

---
+++ precache-manifest.c36a53110e483b85a388963006b6b7b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d63bf0f64fe13092b7f",
+    "url": "/dead-guessr/static/css/main.872772a6.chunk.css"
+  },
+  {
+    "revision": "2d63bf0f64fe13092b7f",
+    "url": "/dead-guessr/static/js/main.2d63bf0f.chunk.js"
+  },
+  {
+    "revision": "569bd20778a3c6b38065",
+    "url": "/dead-guessr/static/js/1.569bd207.chunk.js"
+  },
+  {
+    "revision": "66b031f666ebd0910522",
+    "url": "/dead-guessr/static/js/runtime~main.66b031f6.js"
+  },
+  {
+    "revision": "cabadc3bf55a0dba32c01979d1ce6a4d",
+    "url": "/dead-guessr/index.html"
+  }
+];
